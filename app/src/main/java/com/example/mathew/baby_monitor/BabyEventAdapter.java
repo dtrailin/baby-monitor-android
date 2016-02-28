c3a706490a88f8dc66ef2a4124c87a02ddddf99f
Denis Trailin <denis.trailin@gmail.com>
2016-02-28 06:59:20 -0800

    added event listener for press

--- app/src/main/java/com/example/mathew/baby_monitor/BabyEventAdapter.java
+++ app/src/main/java/com/example/mathew/baby_monitor/BabyEventAdapter.java
@@ -3,11 +3,15 @@ package com.example.mathew.baby_monitor;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+
+import com.activeandroid.query.Select;
+import com.activeandroid.query.Update;
 
 /**
  * Created by denis on 28/02/16.
@@ -30,15 +34,25 @@ public class BabyEventAdapter  extends CursorAdapter {
     // The bindView method is used to bind all data to a given view
     // such as setting the text on a TextView.
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, Context context, final Cursor cursor) {
         // Find fields to populate in inflated template
         TextView reason = (TextView) view.findViewById(R.id.eventName);
         TextView date = (TextView) view.findViewById(R.id.tvEventTime);
         CheckBox checkBox = (CheckBox) view.findViewById(R.id.hasBeenSeen);
         // Extract properties from cursor
         String name = cursor.getString(cursor.getColumnIndexOrThrow(REASON));
-        String priority = cursor.getString(cursor.getColumnIndexOrThrow("timestamp"));
+        final String priority = cursor.getString(cursor.getColumnIndexOrThrow("timestamp"));
         boolean responded = cursor.getInt(cursor.getColumnIndexOrThrow("responded")) > 0;
+
+        checkBox.setPressed(responded);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test","did the thing");
+               CheckBox checkBox = (CheckBox)v;
+                new Update(BabyEvent.class).set("responded",checkBox.isPressed()).where("timestamp="+priority);
+            }
+        });
         // Populate fields with extracted properties
         reason.setText(name);
         date.setText(String.valueOf(priority));
